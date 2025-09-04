4070436e31b3646da85e48a05b89651ce77d89ee
mert <mert.mustehlil@asiste.com.tr>
2025-09-04 17:37:26 +0300

    language set

--- src/sdk/locale/index.ts
+++ src/sdk/locale/index.ts
@@ -1,7 +1,11 @@
 import zh from './zh';
 import en from './en';
+import tr from './tr';
+import de from './de';
 
 export const i18nMap: any = {
     'zh-CN': zh,
     'en-US': en,
+    'tr-TR': tr,
+    'de-DE': de,
 };
